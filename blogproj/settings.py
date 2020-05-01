b8bf694c4665af07c2b464ef39a1cd91ec68c419
Temir Makhmudov <tm@elev8it.be>
2020-05-01 19:12:12 +0200

    Completed module

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'mainapp',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,12 @@ USE_L10N = True
 USE_TZ = True
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_URL = 'login'
+
+LOGIN_REDIRECT_URL = 'index'
+
+AUTH_USER_MODEL = 'users.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
